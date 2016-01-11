89b09ef871c749a1f1f4cbf5018524e802f2296c
Son Nguyen <sonnguyen@Sons-MacBook-Pro.local>
2016-01-10 19:22:30 -0800

    Completed page

---
+++ web/index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+	<title>Son Nguyen's Resume</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/bootstrap-theme.min.css" type="text/css" />
+	<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
+	<link rel="stylesheet" href=" mycss.css" type="text/css" />
+
+    </head>
+    <body class="container" style="background-color: rgb(237, 237, 237)">
+	<div class="thumbnail" style="position: relative; top: 70px">
+	    <div id="name">Son Nguyen</div>
+
+	    <hr>
+
+	    <div class="row container">
+		<div id="leftSide" class="col-sm-9">
+		    <h4>ABOUT ME</h4>
+		    <p>Studying computer science is not about getting a degree; it’s about learning a tool that I can use to solve many different problems in life. Therefore, I decided to go to the U.S to pursuit Bachelor degree in Computer Science at San Jose State University. I have had a lasting question on how I can apply the knowledge accumulated during the years in college to solve problems. I’m self-motivated and always eager to learn new things from other people. I’m inspired by many successful startups like Watsi and Wikipedia on how meaningful their products are to the world.
+		    </p>
+
+		    <hr>
+
+		    <h4>EDUCATION</h4>
+		    <div class="row">
+			<div class="col-md-6">
+			    <div id="schoolYear">2012-2014</div>
+			    <blockquote id="deanzaCourses">
+				<p id="school"><img src="img/deanza.jpg" style="width: auto; height: 60px" class="img-responsive" />De Anza College</p>
+				<p id="courseList">Courses</p>
+			    </blockquote>
+			</div>
+
+			<div class="col-md-6">
+			    <div id="schoolYear">2014-2016</div>
+			    <blockquote id="sjsuCourses">
+				<p id="school"><img src="img/sjsu.jpg" style="width: auto; height: 60px" class="img-responsive" />San Jose State University</p>
+				<p id="courseList">Courses</p>
+			    </blockquote>
+			</div>
+		    </div>
+
+
+		    <hr>
+
+		    <h4>EXPERIENCES</h4>
+		    <div id="experiences" class="row">
+			<div class="col-md-3">
+			    <p><strong>Full-stack Engineering Intern at PymWym</strong><br>11/2015 - current</p>
+
+			</div>
+			<div class="col-md-9">
+			    <ul>
+				<li>Implemented responsive web pages from scratch using HTML, CSS, Javascript, jQuery, and PHP</li>
+				<li>Worked with another intern to integrate fully functional web pages</li>
+			    </ul>
+			</div>
+		    </div>
+		    <br/>
+		    <div id="experiences" class="row">
+			<div class="col-md-3">
+			    <p><strong>Teaching Assistant at San Jose State University</strong><br>9/2015 - current</p>
+
+			</div>
+			<div class="col-md-9">
+			    <ul>
+				<li>Grade the assignments and develop grading standards</li>
+				<li>Establish a working relationship with the professor and effective communication with other students</li>
+			    </ul>
+			</div>
+		    </div>
+
+		    <hr>
+
+		    <h4>REWARDS</h4>
+		    <div id="experiences" class="row">
+			<div class="col-md-3">
+			    <p><strong>Second place winners of the Yahoo! Hackathon at San Jose State University</strong><br></p>
+
+			</div>
+			<div class="col-md-9">
+			    <ul>
+				<li>Conceptualized and simulated a system that categorizes, displays, and automatically responds to events across a distributed system in real time</li>
+			    </ul>
+			</div>
+		    </div>
+
+		    <hr>
+
+		    <h4>PROJECTS</h4>
+		    <div id="projects" class="row">
+		    </div>
+
+		</div>
+
+		<div id="rightSide" class="col-sm-3">
+		    <a href="SonNguyenResume.pdf"><img src="img/resume.png" class="img-responsive" /></a>
+		    <h4>CONTACT</h4>
+		    <div id="contactInfo" class="row">
+			<p>
+			    <a href="tel:408-478-1500">
+				<img src="img/cellphone.png" style="width: 50px; height: 50px" />
+			    </a>
+			    <a href="tel:408-478-1500">408-478-1500</a>
+			</p>
+			<p>
+			    <a href="mailto:sonngnam@gmail.com">
+				<img src="img/email.png" style="width: 50px; height: 50px" />
+			    </a>
+			    <a href="mailto:sonngnam@gmail.com">sonngnam@gmail.com</a>
+			</p>
+			<p>
+			    <a href="https://www.linkedin.com/in/sonngnam">
+				<img src="img/linkedin.png" style="width: 50px; height: 50px" />
+			    </a>
+			    <a href="https://www.linkedin.com/in/sonngnam">Linkedin</a>
+			</p>
+			<p>
+			    <a href="https://github.com/son1nguyen">
+				<img src="img/github.png" style="width: 50px; height: 50px" />
+			    </a>
+			    <a href="https://github.com/son1nguyen">GitHub</a>
+			</p>
+		    </div>
+
+		    <hr>
+
+		    <h4>SKILLS</h4>
+		    <div id="programmingLanguage" class="row">
+			<div class="col-md-6 col-sm-12 col-xs-6">
+			    <img src="img/java.png" />
+			</div>
+			<div class="col-md-6 col-sm-12 col-xs-6">
+			    <img src="img/c.png" />
+			</div>
+			<div class="col-md-6 col-sm-12 col-xs-6">
+			    <img src="img/swift.png" />
+			</div>
+			<div class="col-md-6 col-sm-12 col-xs-6">
+			    <img src="img/javascript.png" />
+			</div>
+			<div class="col-md-6 col-sm-12 col-xs-6">
+			    <img src="img/sql.png" />
+			</div>
+			<div class="col-md-6 col-sm-12 col-xs-6">
+			    <img src="img/html&css.png" />
+			</div>
+		    </div>
+		</div>
+	    </div>
+
+
+	</div>
+
+
+
+	<!--JQUERY-->
+	<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+	<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+
+	<!--BOOTSTRAP-->
+	<script src="js/bootstrap.min.js"></script>
+	<script src="myscript.js"></script>
+    </body>
+</html>
---
+++ web/myscript.js
@@ -0,0 +1,139 @@
+var deanzaCourses = [
+    'Introduction to Programming in C',
+    'Intermediate Programming in C',
+    'Assembly Language',
+    'Discrete Mathematics',
+    'Intermediate Java Programming',
+    'Advanced Java Programming'
+];
+
+var sjsuCourses = [
+    'Object Oriented Design and Analysis',
+    'Data Structures & Algorithms',
+    'Programming Paradigms',
+    'Database Management System',
+    'Formal Languages & Computability',
+    'Software Engineering'
+];
+
+var projects = [
+    {
+	name: 'Car Configuration (06/2014)',
+	image: 'img/car.png',
+	description:
+		[
+		    'Developed a car configuration website using J2EE, Servlet, JSP, Maven, and Bootstrap',
+		    'Scaled the website by using multithreaded servers, synchronized data, and created a customized exception handler'
+		]
+    },
+    {
+	name: 'Bank System (12/2014)',
+	image: 'img/banking.gif',
+	description:
+		[
+		    'Created JDBC application that simulates functionalities of a bank',
+		    'Grouped SQL statements into transaction to maintain the atomicity'
+		]
+    },
+    {
+	name: 'Operating System Simulation (03/2015)',
+	image: 'img/os.png',
+	description:
+		[
+		    'Implemented different scheduler algorithms of the operating system in Java',
+		    'Created a multithreaded application in C and specified critical regions to avoid race condition as well as deadlock'
+		]
+    },
+    {
+	name: 'Chat Room (5/2014)',
+	image: 'img/chatApp.png',
+	description:
+		[
+		    'Developed a Java web-based chat application that allows multiple people to join in a conversation',
+		    'Used Javascript, JSON, and RESTful web service for the communication between the server and clients'
+		]
+    },
+    {
+	name: 'Airport Management System (12/2013)',
+	image: 'img/hashTable.png',
+	description:
+		[
+		    'Led a team of 4 to build a C application that manage a list of airports from around the world',
+		    'Implemented binary search tree and hash table in C to manage an airport list efficiently and meet different requirements from customers',
+		    'Made the hash table resizable to maintain the efficiency and adapt with an increasing number of airports'
+		]
+    }
+];
+
+
+$(document).ready(function () {
+    loadDeanzaCourses();
+    loadSJSUCourses();
+    loadProjects();
+    addHoverImg();
+});
+
+function loadDeanzaCourses() {
+    $.each(deanzaCourses, function (index) {
+	var footer = $('<footer>').text(deanzaCourses[index]);
+	$('#deanzaCourses').append(footer);
+    });
+}
+
+function loadSJSUCourses() {
+    $.each(sjsuCourses, function (index) {
+	var footer = $('<footer>').text(sjsuCourses[index]);
+	$('#sjsuCourses').append(footer);
+    });
+}
+
+function loadProjects() {
+    for (var i = 0; i < projects.length; i++) {
+	var divRow = $('<div>').addClass('row').css({
+	    marginTop: '10px'
+	});
+	var colSm3 = $('<div>').addClass('col-sm-3').css({
+	    textAlign: 'center'
+	});
+	var colSm9 = $('<div>').addClass('col-sm-9');
+
+	var img = $('<img src="' + projects[i].image + '" />');
+	colSm3.append('<img src="' + projects[i].image + '" />');
+	var ul = $('<ul>');
+	for (var j = 0; j < projects[i].description.length; j++) {
+	    ul.append('<li>' + projects[i].description[j] + '</li>');
+	}
+	colSm9.append(ul);
+
+	divRow.append(colSm3);
+	divRow.append(colSm9);
+
+	$('#projects').append(divRow);
+	$('#projects').append($('<hr>'));
+    }
+}
+
+function addHoverImg() {
+    var imgWidth = '150px';
+    $('#projects img').each(function () {
+	var width = $(this).css('width');
+	$(this).hover(function () {
+	    $(this).animate({
+		width: '350px',
+		zIndex: '1',
+		height: 'auto'
+	    }, 300);
+	}, function () {
+	    $(this).animate({
+		width: imgWidth,
+		height: 'auto'
+	    }, 100);
+	    $(this).css({
+		width: imgWidth,
+		height: 'auto'
+	    });
+	});
+    });
+}
+
+
